bcbee9015eb1f1003e0aebc8e59e3f21cef874f6
nikros586 <artemnaumov586@gmail.com>
2025-11-22 19:43:19 +0300

    dz 1.1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        byte a = 3;
+        short b = 5;
+        long c = 182L;
+        float f = 543.5F;
+        double h = 443.3F;
+
+        System.out.println("Значение переменной a s tipom byte ravno " + a);
+        System.out.println("Значение переменной b s tipom short ravno " + b);
+        System.out.println("Значение переменной c s tipom long ravno " + c);
+        System.out.println("Значение переменной f s tipom float ravno " + f);
+        System.out.println("Значение переменной h s tipom double ravno " + h);
+
+
+    }
+}
