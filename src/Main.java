823b56ebabb246d1ec3108cbd2339061b673f71f
nikros586 <artemnaumov586@gmail.com>
2025-12-11 18:22:55 +0300

    dz 3.2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 11; i++) {
-            System.out.println(i);
+        for (int i = 0; i < 21; i++) {
+            if (i%2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
