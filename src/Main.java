de709565dfebba2c1b9a1f47963953b2f58df8bc
nikros586 <artemnaumov586@gmail.com>
2025-12-20 19:48:11 +0300

    dz 5.2

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] yui = {2, 5, 3, 8, 6};
+        int [] yui = {2, -5, 3, 8, -6};
         float sum = 0;
         for(int i = 0; i < yui.length; i++){
-              sum += yui[i];
+              if(yui[i] < 0){
+                  yui[i] *= -1;
+              }
         }
-        sum = sum / yui.length;
-        System.out.println(sum);
+        System.out.println(Arrays.toString(yui));
     }
 }
