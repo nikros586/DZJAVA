de0ee3ce10a391c2eaf8dae665aad0111280bcc2
nikros586 <artemnaumov586@gmail.com>
2025-12-20 19:35:58 +0300

    dz 4.3

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        do{
-            if(i%7 == 0){
-                System.out.println(i);
-            }
+        int i = 2, sum = 1;
+        while (i != 100){
+            sum += i;
             i++;
-        }while(i < 100);
+        }
+        System.out.println(sum);
 
     }
 }
