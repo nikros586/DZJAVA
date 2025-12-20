632028a1309b8ec2103aa16f72467051f20ac050
nikros586 <artemnaumov586@gmail.com>
2025-12-20 19:51:13 +0300

    dz 5.3

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] yui = {2, -5, 3, 8, -6};
+        int [][] yui = {{2, 5, 3, 8, 6},
+                        {1, 4, 2, 7, 5}};
         float sum = 0;
         for(int i = 0; i < yui.length; i++){
-              if(yui[i] < 0){
-                  yui[i] *= -1;
-              }
+            for(int j = 0; j < yui[i].length; j++){
+                sum += yui[i][j];
+            }
+            System.out.println(sum);
+            sum = 0;
         }
-        System.out.println(Arrays.toString(yui));
+
     }
 }
