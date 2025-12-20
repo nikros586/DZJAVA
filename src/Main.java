a6b06b45a7c5a92931741be8b825c014cad57db1
nikros586 <artemnaumov586@gmail.com>
2025-12-20 19:44:35 +0300

    dz 5.1

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 1;
-        int g = (int)(Math.random() * 20);
-        while (g != 15){
-            g = (int)(Math.random() * 20);
-            i++;
+        int [] yui = {2, 5, 3, 8, 6};
+        float sum = 0;
+        for(int i = 0; i < yui.length; i++){
+              sum += yui[i];
         }
-        System.out.println(i);
-
+        sum = sum / yui.length;
+        System.out.println(sum);
     }
 }
