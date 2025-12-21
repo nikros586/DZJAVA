8fd0c189c0873ac8f58bf94733c7f54e5e8d9f72
nikros586 <artemnaumov586@gmail.com>
2025-12-21 15:11:18 +0300

    dz 5.5

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int [] yui = {2, 5, 3, 8, 6};
-        int sum = 0, otv = 0;
+        String otv = "NO";
         for(int i = 0; i < yui.length; i++){
-            if(sum < yui[i]){
-                sum = yui[i];
-                otv = i;
+            for(int j = i+1; j < yui.length; j++){
+                if(yui[i] == yui[j]){
+                    otv = "YES";
+                }
             }
         }
         System.out.println(otv);
