d2872addabe637b50a02e48a94f17b30c2180ba3
nikros586 <artemnaumov586@gmail.com>
2025-12-27 19:39:44 +0300

    dz 6.1

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int a = scanner.nextInt();
-
-        if(a >= 2 && a <=6){
-            System.out.println("Дeтский сад");
-        }
-        else if(a >= 7 && a <=17){
-            System.out.println("Школа");
-        }
-        else if (a >= 18 && a <=24){
-            System.out.println("университет");
-        }
-        else if(a >= 24){
-            System.out.println("работа");
-        }
-        else {
-            System.out.println("You very small");
-        }
-
+        int [] mas = {5, 7, 3, 8};
     }
+
 }
