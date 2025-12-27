932940420c1f8ea08547185f75222744b1de5c8e
nikros586 <artemnaumov586@gmail.com>
2025-12-27 19:43:31 +0300

    dz 6

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,17 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int [] mas = {5, 7, 3, 8};
+
+        for(int i : mas){
+            System.out.print(i + " ");
+        }
+
+        System.out.print("\n");
+
+        for(int i = 0; i < mas.length; i++){
+            mas[i] *= 2;
+            System.out.print(mas[i] + " ");
+        }
     }
 
 }
