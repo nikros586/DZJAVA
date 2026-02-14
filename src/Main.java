709772d5918aa74c57be65c4ab914619fdaf1be0
nikros586 <artemnaumov586@gmail.com>
2026-02-14 19:45:59 +0300

    dz 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,57 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] yui = {2, 5, 3, 8, 6};
-        String otv = "NO";
-        for(int i = 0; i < yui.length; i++){
-            for(int j = i+1; j < yui.length; j++){
-                if(yui[i] == yui[j]){
-                    otv = "YES";
-                }
-            }
-        }
-        System.out.println(otv);
+        Employee employee = new Employee(10, "Name", 10);
+        System.out.println(employee.getAge());
+        System.out.println(employee.getName());
+        System.out.println(employee.getSalary());
 
     }
 }
+
+class Person{
+    private String name;
+    private int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public int setAge(int ages){
+        age = ages;
+        return age;
+    }
+    public String setName(String names){
+        name = names;
+        return name;
+    }
+}
+class Employee extends Person{
+    private int salary;
+    public Employee(int salary, String name, int age){
+        super(name, age);
+        this.salary = salary;
+
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
+    public int getSalary( int salarys){
+        salary = salarys;
+        return salary;
+    }
+}
+
+
