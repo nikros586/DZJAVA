8aad7652f8cfbe7040ad79de7687cc99873db1fb
nikros586 <artemnaumov586@gmail.com>
2026-03-21 19:34:20 +0300

    dz 2.6.1

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,15 @@
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> todolist = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        while (true){
-            System.out.println("Что вы хотите сделать?");
-            int a = scanner.nextInt();
-            scanner.nextLine();
-            if(a == 1){
-                String b = scanner.nextLine();
-                todolist.add(b);
-            }
-            else {
-                int c = scanner.nextInt();
-                todolist.remove(c);
-            }
-            System.out.println(todolist);
-        }
+        Set<String> user = new HashSet<>();
+        user.add("user6");
+        System.out.println(user);
+        System.out.println(user.contains("User1"));
+
+
+
 
     }
 
