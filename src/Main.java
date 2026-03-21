d4f393c8fe19de1c4fb81d6e762a63300efa3689
nikros586 <artemnaumov586@gmail.com>
2026-03-21 19:44:37 +0300

    dz 2.6.3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String qwe = "one two one";
+        String qwe = "Guest1 neguest ne ne";
         Set<String> qwe1 = new TreeSet<>();
         List<String> qwe2 = Arrays.asList(qwe.split("\\s+"));
         for (String i : qwe2){
